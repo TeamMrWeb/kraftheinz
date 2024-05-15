565df8bdfecac4b7bea1f83037841b01a8ea8859
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-15 20:31:02 -0300

    feat: add InputGroup component

---
+++ src/components/InputGroup.jsx
@@ -0,0 +1,16 @@
+import Input from "./Input"
+
+export default function InputGroup({
+  containerClassName,
+  label,
+  inputProps,
+  inputClassName,
+  htmlFor
+}) {
+  return (
+    <div className={`flex flex-col gap-1 ${containerClassName}`}>
+      <label htmlFor={htmlFor}>{label}</label>
+      <Input props={inputProps} className={inputClassName} />
+    </div>
+  )
+}
