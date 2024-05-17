17e0657f0dc64ea483837400ddae8c0b298226da
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-17 14:18:08 -0300

    refactor: update logo animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,8 @@
-import { useEffect } from "react"
-import { useRef } from "react"
-
 export default function Hero() {
-  const logoLosRef = useRef()
-  const logoHeroesRef = useRef()
-  const logoDelRef = useRef()
-  const logoVeranoRef = useRef()
-
-  useEffect(() => {
-    setTimeout(() => {
-      logoLosRef.current.style.opacity = "1"
-    }, 500)
-    setTimeout(() => {
-      logoHeroesRef.current.style.opacity = "1"
-    }, 600)
-    setTimeout(() => {
-      logoDelRef.current.style.opacity = "1"
-    }, 700)
-    setTimeout(() => {
-      logoVeranoRef.current.style.opacity = "1"
-    }, 800)
-  }, [])
-
   return (
     <div className="w-full flex relative bg-black h-[256px] md:h-[400px]">
       <div
-        className="w-full bg-local bg-[center_bottom_-90vw] md:bg-[center_top_-15vw] lg:bg-[center_top_-25vw] xl:bg-[center_top_-30vw] 2xl:bg-[center_top_-35vw] bg-no-repeat bg-cover relative flex justify-center items-center"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-        data-aos-once="true"
-        data-aos-delay="800"
+        className="w-full bg-local bg-[center_bottom_-90vw] md:bg-[center_top_-15vw] lg:bg-[center_top_-25vw] xl:bg-[center_top_-30vw] 2xl:bg-[center_top_-35vw] bg-no-repeat bg-cover relative flex justify-center items-center z-0 background"
         style={{
           backgroundImage: `url(${"/assets/images/kh-heroes-poster.webp"})`
         }}
@@ -38,48 +11,36 @@ export default function Hero() {
         <div className="relative flex justify-center items-center h-min">
           <div className="flex flex-col items-center relative">
             <img
-              className="w-[30px] md:w-[50px] object-cover bg-center bg-no-repeat bg-cover m-auto z-10 absolute top-0 left-[40%] logo-los opacity-0"
+              className="w-[30px] md:w-[50px] object-cover bg-center bg-no-repeat bg-cover m-auto z-10 absolute top-0 left-[40%] logo-los"
               src="/assets/images/los.png"
               alt="Los"
-              ref={logoLosRef}
             />
             <img
-              className="w-[160px] md:w-[250px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-20 mt-2 md:mt-4 logo-heroes opacity-0"
+              className="w-[160px] md:w-[250px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-20 mt-2 md:mt-4 logo-heroes"
               src="/assets/images/heroes.png"
               alt="heroes"
-              ref={logoHeroesRef}
             />
             <img
-              className="w-[30px] md:w-[50px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-10 absolute top-[15%] logo-del opacity-0"
+              className="w-[30px] md:w-[50px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-10 absolute top-[15%] logo-del"
               src="/assets/images/del.png"
               alt="del"
-              ref={logoDelRef}
             />
             <img
-              className="w-[160px] md:w-[250px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-10 mt-4 md:mt-6 logo-verano opacity-0"
+              className="w-[160px] md:w-[250px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-10 mt-4 md:mt-6 logo-verano"
               src="/assets/images/verano.png"
               alt="verano"
-              ref={logoVeranoRef}
-            />
-            <img
-              className="w-[150px] md:w-[190px] absolute object-cover"
-              src="/assets/images/sun-background.webp"
-              alt=""
-              data-aos="fade-up"
-              data-aos-duration="1000"
-              data-aos-once="true"
-              data-aos-delay="800"
             />
           </div>
         </div>
         <img
-          className="w-[150px] md:w-[190px] object-cover mt-2"
+          className="w-[150px] md:w-[190px] absolute object-cover background-sun"
+          src="/assets/images/sun-background.webp"
+          alt=""
+        />
+        <img
+          className="w-[150px] md:w-[190px] object-cover mt-2 kraftheninz-logo"
           src="/assets/images/kraftheinz-logo.png"
           alt=""
-          data-aos="fade-up"
-          data-aos-duration="1000"
-          data-aos-once="true"
-          data-aos-delay="800"
         />
       </div>
     </div>
