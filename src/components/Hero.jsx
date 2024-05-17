4eacdd375b22e08e62bcd39defe9820e16eaf968
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-17 08:50:11 -0300

    refactor: add new hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,34 @@
 export default function Hero() {
   return (
-    <div className="w-full flex relative">
+    <div className="w-full flex relative bg-black h-[256px] md:h-[400px]">
       <div
-        className="w-full h-[256px] md:h-[400px] bg-local md:bg-[center_top_-10vw] xl:bg-[center_top_-35vw] bg-no-repeat bg-cover relative flex justify-center items-center"
+        className="w-full bg-local bg-[center_bottom_-90vw] md:bg-[center_top_-15vw] lg:bg-[center_top_-25vw] xl:bg-[center_top_-30vw] 2xl:bg-[center_top_-35vw] bg-no-repeat bg-cover relative flex justify-center items-center background"
         style={{
           backgroundImage: `url(${"/assets/images/kh-heroes-poster.webp"})`
         }}
-      >
-        <div className="absolute m-auto flex flex-col justify-center items-center w-full md:mb-10">
-          <div className="relative m-auto flex justify-center items-center h-min">
+      ></div>
+      <div className="h-full absolute m-auto flex flex-col justify-center items-center w-full md:mb-10">
+        <div className="relative flex justify-center items-center h-min">
+          <div className="flex flex-col items-center relative">
             <img
-              className="w-[200px] md:w-[300px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-10 logo"
-              src="/assets/images/los-heroes-del-verano-logo.png"
-              alt="Logo de Los Heroes del Verano"
+              className="w-[30px] md:w-[50px] object-cover bg-center bg-no-repeat bg-cover m-auto z-10 absolute top-0 left-[40%] logo-los"
+              src="/assets/images/los.png"
+              alt="Los"
+            />
+            <img
+              className="w-[160px] md:w-[250px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-20 mt-2 md:mt-4 logo-heroes"
+              src="/assets/images/heroes.png"
+              alt="heroes"
+            />
+            <img
+              className="w-[30px] md:w-[50px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-10 absolute top-[15%] logo-del"
+              src="/assets/images/del.png"
+              alt="del"
+            />
+            <img
+              className="w-[160px] md:w-[250px] object-cover bg-center bg-no-repeat bg-cover m-auto inset-0 z-10 mt-4 md:mt-6 logo-verano"
+              src="/assets/images/verano.png"
+              alt="verano"
             />
             <img
               className="w-[150px] md:w-[190px] absolute object-cover"
@@ -20,12 +36,12 @@ export default function Hero() {
               alt=""
             />
           </div>
-          <img
-            className="w-[150px] md:w-[190px] object-cover"
-            src="/assets/images/kraftheinz-logo.png"
-            alt=""
-          />
         </div>
+        <img
+          className="w-[150px] md:w-[190px] object-cover mt-2"
+          src="/assets/images/kraftheinz-logo.png"
+          alt=""
+        />
       </div>
     </div>
   )
