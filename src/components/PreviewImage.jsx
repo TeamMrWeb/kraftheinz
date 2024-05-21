f8c155e8f735ed545eb08c0d95bbfaa494696447
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-21 08:46:18 -0300

    feat(PrewviewImage): improve system

--- src/components/PreviewImage.jsx
+++ src/components/PreviewImage.jsx
@@ -1,17 +1,38 @@
-function PreviewImage({ previewImage, removePreviewImage }) {
+function PreviewImage({
+  previewImage,
+  removePreviewImage,
+  alt,
+  handleShowPreviewImage
+}) {
   return (
     <div className="relative">
-      <div
-        className="bg-black p-3 rounded-full flex justify-center items-center absolute top-2 right-2 z-10 hover:bg-crimson cursor-pointer"
+      <button
+        className="p-98 rounded-full flex justify-center items-center absolute top-2 left-2 z-10 cursor-pointer"
         onClick={removePreviewImage}
+        type="button"
       >
         <img
-          className="size-5"
+          className="size-6 md:size-7"
+          src="/assets/images/trash-icon.png"
+          alt="Icono de borrar"
+        />
+      </button>
+      <button
+        className="p-98 rounded-full flex justify-center items-center absolute top-2 right-2 z-10 cursor-pointer"
+        onClick={handleShowPreviewImage}
+        type="button"
+      >
+        <img
+          className="size-5 md:size-6"
           src="/assets/images/close-icon.png"
           alt="Icono de cerrar"
         />
-      </div>
-      <img src={previewImage} alt="Recibo de compra" />
+      </button>
+      <img
+        src={previewImage}
+        alt={alt}
+        className="size-60 md:size-96 object-cover"
+      />
     </div>
   )
 }
