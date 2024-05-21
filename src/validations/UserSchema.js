a0c2c9e1d15103b554f0ab9a04e4511ab845ef30
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-21 08:47:08 -0300

    refactor(UserSchema): refactor fields

--- src/validations/UserSchema.js
+++ src/validations/UserSchema.js
@@ -4,41 +4,21 @@ const MAX_FILE_SIZE = 5000000
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"]
 
 export const userSchema = z.object({
-  fullname: z
-    .string()
-    .min(3, {
-      message: "El nombre y apellido debe contener al menos 3 caracteres"
-    })
-    .max(80, {
-      message: "El nombre y apellido debe contener menos de 80 caracteres"
-    }),
+  fullname: z.string().min(3).max(80),
   phone: z
     .string()
-    .min(5, {
-      message: "El número de teléfono debe tener al menos 5 caracteres"
-    })
-    .max(10, {
-      message: "El número de teléfono debe tener hasta 10 caracteres"
-    })
+    .min(7)
+    .max(18)
     .refine(phone => Number(phone)),
-  postulation: z
-    .string()
-    .min(10, {
-      message: "La postulación debe contener al menos 10 caracteres"
-    })
-    .max(500, {
-      message: "La postulación debe contener menos de 500 caracteres"
-    }),
-  image: z
+  email: z.string().email(),
+  postulation: z.string().min(3).max(500),
+  hero: z
     .any()
-    .refine(file => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
-    .refine(
-      file => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-      "Solo se admiten formatos .jpg, .jpeg, y .png"
-    ),
-  termsAndConditions: z
-    .string({
-      message: "Debes aceptar los términos y condiciones para continuar"
-    })
-    .transform(value => value === "on")
+    .refine(file => file?.size <= MAX_FILE_SIZE)
+    .refine(file => ACCEPTED_IMAGE_TYPES.includes(file?.type)),
+  receipt: z
+    .any()
+    .refine(file => file?.size <= MAX_FILE_SIZE)
+    .refine(file => ACCEPTED_IMAGE_TYPES.includes(file?.type)),
+  termsAndConditions: z.string().transform(value => value === "on")
 })
